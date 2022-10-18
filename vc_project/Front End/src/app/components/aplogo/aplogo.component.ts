d0aec2c88c74e21aa44ed8dc003e0e42cc3896b4
ValeriaCommatteo <valc2012@hotmail.com>
2022-10-18 14:46:51 -0300

    Front end

---
+++ vc_project/Front End/src/app/components/aplogo/aplogo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-aplogo',
+  templateUrl: './aplogo.component.html',
+  styleUrls: ['./aplogo.component.css']
+})
+export class AplogoComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
+
+}
