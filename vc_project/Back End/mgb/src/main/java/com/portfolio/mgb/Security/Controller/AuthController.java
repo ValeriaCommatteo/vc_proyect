34979313be983d162acb84b47163341c54a5ca98
ValeriaCommatteo <valc2012@hotmail.com>
2022-10-31 11:11:15 -0300

    cambios en el croos

--- vc_project/Back End/mgb/src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
+++ vc_project/Back End/mgb/src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "https://fendprueba.web.app"})
+@CrossOrigin()
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
